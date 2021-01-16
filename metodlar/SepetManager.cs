00f34613202ecb7cc9fff4656212b3ac86a5d3f2
aly ekrm <alyekrm@hotmail.com>
2021-01-16 23:50:38 +0300

    16 ocak ders sonu

---
+++ metodlar/SepetManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi:"+urun.Adi);
+        }
+        public void Ekle2()
+    }
+}
